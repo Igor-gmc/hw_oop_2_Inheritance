923b991d1504c0f61879b9bef7693014fecebb88
Igor_gl <galackov@gmail.com>
2025-10-11 14:45:46 +0400

    Init commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from classes.student import Student
+from classes.mentor import Mentor
+
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+ 
+cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor.courses_attached += ['Python']
+ 
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+ 
+print(best_student.grades)
