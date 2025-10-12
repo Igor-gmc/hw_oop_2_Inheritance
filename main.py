b763e4468ca9fb6c64197c9402ea3ba711b6ba43
Igor_gl <galackov@gmail.com>
2025-10-12 12:44:40 +0400

    task_03 redy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from turtle import st
 from classes.student import Student
-from classes.mentor import Reviewer, Lecturer, Mentor
+from classes.mentor import Reviewer, Lecturer
 
 lecturer = Lecturer('Иван', 'Иванов')
 reviewer = Reviewer('Пётр', 'Петров')
@@ -11,14 +10,18 @@ student.courses_in_progress += ['Python', 'Git']
 student.finished_courses += ['Введение в программирование']
 student.finished_courses += ['Java']
 
+# Записываем курсы для лекеторов и проверяющих
 lecturer.courses_attached += ['Python', 'C++']
 reviewer.courses_attached += ['Python', 'C++']
+
+# Выставляем оценки студенту
 reviewer.rate_hw(student, 'Python', 10)  # None
 reviewer.rate_hw(student, 'Python', 9)   # None
 reviewer.rate_hw(student, 'Python', 8)   # None
 reviewer.rate_hw(student, 'Java', 9)     # None
 reviewer.rate_hw(student, 'Java', 10)     # None
  
+# Выставляем оценки лектору
 student.rate_lecture(lecturer, 'Python', 7)   # None
 student.rate_lecture(lecturer, 'Java', 8)     # Ошибка
 student.rate_lecture(lecturer, 'С++', 8)      # Ошибка
